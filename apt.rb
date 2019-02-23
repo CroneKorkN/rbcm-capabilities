4e61a376eb1b53cb86d15c877824b8d8401c41ec
mwie <mwiegand@seibert-media.net>
2019-02-23 16:44:24 +0100

    ok

--- apt.rb
+++ apt.rb
@@ -2,9 +2,10 @@ def apt install:,
     remove: nil,
     purge: false
   if install
+    run "apt-get update",
     [install].flatten.each do |pkg|
-      run "apt-get install -y #{install}",
-        check: "dpkg-query -l #{install}"
+      run "apt-get install -y #{pkg}",
+        check: "dpkg-query -l #{pkg}"
     end
   elsif remove
     run "apt-get remove -y #{remove} #{'--purge' if purge}",
